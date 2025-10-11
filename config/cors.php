0a61ea5d6968071eb856149bc27cf19433a8bb11
Njokuocha Francis <francisnjokuocha@gmail.com>
2025-10-11 11:53:20 +0100

    Change in config\cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,12 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:3000', 
-        'http://10.171.123.92:3000', 
-        'http://localhost',
-        'https://dynamiteapi.vercel.app'
-    ],
+    'allowed_origins' => ["*"],
     'allowed_headers' => ['*'],
     'supports_credentials' => false, // no cookies needed here
 
